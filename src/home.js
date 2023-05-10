0783dadd6d94715385a20fad0923bc2a696f8cff
Olguta Fratila <fratilaolguta150@gmail.com>
2023-05-10 13:32:32 +0300

    add replaceChildren function to replace the page's content with the actual function

--- src/home.js
+++ src/home.js
@@ -1,5 +1,10 @@
+import { nav } from "./nav";
+
 function home() {
     const container = document.getElementById('content');
+
+    container.replaceChildren();
+    nav();
 
     const homePage = document.createElement('div');
     homePage.classList.add('home');
@@ -7,7 +12,7 @@ function home() {
         <div class="heading">Welcome To Cip</div>
         <div class="main">
         <div class="description">Cip Restaurant specializes in delicious food featuring fresh ingredients and masterful preparation by the culinary team. Whether you're ordering a multi-course meal or grabbing a drink and pizza at the bar, Cip's lively, casual yet upscale atmosphere makes it perfect for dining with friends, family, clients and business associates.</div>
-        <img class="img" src="/dist/images/—Pngtree—luxury gold background thai pattern_2455373.jpg" alt="description-background">
+        <img class="img" src="../dist/images/—Pngtree—luxury gold background thai pattern_2455373.jpg" alt="description-background">
         </div>
     `;
 
