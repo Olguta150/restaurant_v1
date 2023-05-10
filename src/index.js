ab80093255a189645b588de1fe8932199d9bc81c
Olguta Fratila <fratilaolguta150@gmail.com>
2023-05-10 13:35:20 +0300

    add switchModule function logic to switch modules

--- src/index.js
+++ src/index.js
@@ -1,21 +1,36 @@
+import './style.css';
 import { home } from "./home";
 import { menu } from "./menu";
 import { nav } from "./nav";
-import { meals } from "./meals-menu";
-import { desserts } from "./desserts-menu";
-import { drinks } from "./drinks-menu";
+import { meals } from "./menu-components/meals-menu";
+import { desserts } from "./menu-components/desserts-menu";
+import { drinks } from "./menu-components/drinks-menu";
 
-nav();
-home();
 // menu();
 
-const switchModule = (function() {
-    const homeBtn = document.querySelector('.home');
-    homeBtn.addEventListener('click', home);
+window.addEventListener('load', () => {
+    nav();
+    home();
+})
 
-    const menuBtn = document.querySelector('.menu');
-    // menuBtn.addEventListener('click', menu);
-    console.log(menu());
+const switchModule = (function() {
+    const container = document.getElementById('content');
+    container.addEventListener('click', (e) => {
+        console.log("e: ", e.target.id);
+
+        if(e.target.id === 'homeBtn') {
+            home();
+        } else if(e.target.id === 'menuBtn') {
+            menu();
+        }
+    })
+    // const homeBtn = document.getElementById('homeBtn');
+    // console.log(homeBtn);
+    // // homeBtn.addEventListener('click', home);
+
+    // const menuBtn = document.getElementById('menuBtn');
+    // // menuBtn.addEventListener('click', menu);
+    // console.log(menuBtn);
 
 })();
 
