b485d4340434ef245dfe58bbc7a4eb37a1d532f5
Olguta Fratila <fratilaolguta150@gmail.com>
2023-05-10 13:46:31 +0300

    change logo class into an id

--- src/nav.js
+++ src/nav.js
@@ -4,7 +4,7 @@ function nav() {
     const navigation = document.createElement('nav');
     navigation.classList.add('nav');
     navigation.innerHTML = `
-        <div class="logo">Cip Restaurant</div>
+        <div id="logo">Cip Restaurant</div>
         <ul>
             <li id="homeBtn">Home</li>
             <li id="menuBtn">Menu</li>
