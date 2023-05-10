b8b0e8733d79446f4961b94ac816cffc1f5b8a6b
Olguta Fratila <fratilaolguta150@gmail.com>
2023-05-10 20:17:03 +0300

    add logic to go to contact page

--- src/index.js
+++ src/index.js
@@ -3,10 +3,7 @@ import { home } from "./home";
 import { menu } from "./menu";
 import { nav } from "./nav";
 import { meals } from "./menu-components/meals-menu";
-import { desserts } from "./menu-components/desserts-menu";
-import { drinks } from "./menu-components/drinks-menu";
-
-// menu();
+import { contact } from './contact';
 
 nav();
 home();
@@ -15,30 +12,11 @@ const switchModule = (function() {
     const container = document.getElementById('content');
 
     container.addEventListener('click', (e) => {
-        console.log("e: ", e.target.id);
-
-        if(e.target.id === 'homeBtn' || e.target.id === 'logo') {
-            home();
-        } else if(e.target.id === 'menuBtn') {
+        if(e.target.id === 'homeBtn' || e.target.id === 'logo') home();
+        if(e.target.id === 'menuBtn') {
             menu();
+            meals();
         }
+        if(e.target.id === 'contactBtn') contact();
     })
-    // const homeBtn = document.getElementById('homeBtn');
-    // console.log(homeBtn);
-    // // homeBtn.addEventListener('click', home);
-
-    // const menuBtn = document.getElementById('menuBtn');
-    // // menuBtn.addEventListener('click', menu);
-    // console.log(menuBtn);
-
 })();
-
-// const mealsOption = document.getElementById('meals');
-// const dessertsOption = document.getElementById('desserts');
-// const drinksOption = document.getElementById('drinks');
-
-// // meals();
-
-// mealsOption.addEventListener('click', meals);
-// dessertsOption.addEventListener('click', desserts);
-// drinksOption.addEventListener('click', drinks);
